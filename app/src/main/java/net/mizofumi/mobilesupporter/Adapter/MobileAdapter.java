d3e88ff021befed9d44d25a4a937ddc5d56c36de
mizofumi0411 <mizofumiya0411@gmail.com>
2016-08-20 04:14:41 +0900

    画像取得元にMbaasを追加
    Mbaasに保存し、次回取得時はMbaasからの取得を優先するようにした

--- app/src/main/java/net/mizofumi/mobilesupporter/Adapter/MobileAdapter.java
+++ app/src/main/java/net/mizofumi/mobilesupporter/Adapter/MobileAdapter.java
@@ -4,12 +4,20 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.nifty.cloud.mb.core.DoneCallback;
+import com.nifty.cloud.mb.core.FetchFileCallback;
+import com.nifty.cloud.mb.core.NCMB;
+import com.nifty.cloud.mb.core.NCMBAcl;
+import com.nifty.cloud.mb.core.NCMBException;
+import com.nifty.cloud.mb.core.NCMBFile;
 
 import net.mizofumi.mobilesupporter.Image.GoogleImageSearch;
 import net.mizofumi.mobilesupporter.Image.GoogleImageSearchListener;
@@ -18,6 +26,7 @@ import net.mizofumi.mobilesupporter.Image.ImageDownloadListener;
 import net.mizofumi.mobilesupporter.Mobile;
 import net.mizofumi.mobilesupporter.R;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 /**
@@ -56,48 +65,76 @@ public class MobileAdapter extends RecyclerView.Adapter<MobileAdapter.ViewHolder
      * あとは、MBassに送信しキャッシュの保存。
      */
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         holder.name.setText(list.get(position).getName());
         holder.model.setText(list.get(position).getModel());
         holder.type.setText(list.get(position).getType());
 
-        new GoogleImageSearch(list.get(position).getModel(), new GoogleImageSearchListener() {
+        NCMBFile ncmbFile = new NCMBFile(list.get(position).getModel()+".png");
+        ncmbFile.fetchInBackground(new FetchFileCallback() {
             @Override
-            public void onPreExec() {
-                holder.loadingMessage.setText("Google検索中...");
+            public void done(byte[] bytes, NCMBException e) {
+                if (e == null) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                    holder.imageView.setImageBitmap(bitmap);
+                    Log.d("MobileAdapter","From MBaas:"+list.get(position).getModel());
+                } else {
+                    Log.d("MobileAdapter","From Google:"+list.get(position).getModel());
+                    new GoogleImageSearch(list.get(position).getModel(), new GoogleImageSearchListener() {
+                        @Override
+                        public void onPreExec() {
+                            holder.loadingMessage.setText("Google検索中...");
+                        }
+
+                        @Override
+                        public void onPostExec(final String imageUrl) {
+
+                            holder.loadingMessage.setText("画像取得中...");
+                            new ImageDownload(imageUrl, new ImageDownloadListener() {
+
+                                @Override
+                                public void onPostExec(Bitmap bitmap) {
+                                    holder.imageView.setImageBitmap(bitmap);
+                                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                                    NCMBFile ncmbFile = new NCMBFile(list.get(position).getModel() + ".png", byteArrayOutputStream.toByteArray(), new NCMBAcl());
+                                    ncmbFile.saveInBackground(new DoneCallback() {
+                                        @Override
+                                        public void done(NCMBException e) {
+                                            if (e == null) {
+                                                Log.d("MobileAdapter", "uploaded");
+                                            } else {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void error(Exception e) {
+                                    holder.loadingProgress.setVisibility(View.GONE);
+                                    holder.loadingMessage.setText("画像取得失敗...");
+                                }
+                            }).download(context);
+
+                        }
+
+                        @Override
+                        public void onBackground() {
+
+                        }
+
+                        @Override
+                        public void error(Exception e) {
+                            holder.loadingProgress.setVisibility(View.GONE);
+                            holder.loadingMessage.setText("Google検索失敗...");
+                        }
+                    }).start();
+                }
             }
+        });
 
-            @Override
-            public void onPostExec(final String imageUrl) {
 
-                holder.loadingMessage.setText("画像取得中...");
-                new ImageDownload(imageUrl, new ImageDownloadListener() {
-
-                    @Override
-                    public void onPostExec(Bitmap bitmap) {
-                        holder.imageView.setImageBitmap(bitmap);
-                    }
-
-                    @Override
-                    public void error(Exception e) {
-                        holder.loadingProgress.setVisibility(View.GONE);
-                        holder.loadingMessage.setText("画像取得失敗...");
-                    }
-                }).download(context);
-
-            }
-
-            @Override
-            public void onBackground() {
-
-            }
-
-            @Override
-            public void error(Exception e) {
-                holder.loadingProgress.setVisibility(View.GONE);
-                holder.loadingMessage.setText("Google検索失敗...");
-            }
-        }).start();
     }
 
     @Override
